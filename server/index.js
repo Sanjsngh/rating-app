80df482ab45c2f9256d03a006a67d7fde0b693b0
Sanjsngh <sanjivanisngh@gmail.com>
2023-09-17 23:37:06 +0530

    Deployment phase

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const path = require('path');
 const authRouter = require('./router/Auth.js');
 const companyRouter = require('./router/Companies.js');
 const userRouter = require('./router/Users.js');
-
 require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 const clientPort = 3000;
@@ -20,6 +19,12 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req,res) => res.sendFile(path.join(__dirname, '..','client', 'build','index.html')));
+}
 app.use(express.static('public'));
 
 app.use('/auth', authRouter);
@@ -29,4 +34,6 @@ app.use('/users', userRouter);
 
 app.listen(PORT, () => {
     console.log(`App is running at http://localhost:${PORT}`);
-  });
+});
+
+// console.log("heyyyyy: " + __dirname);
